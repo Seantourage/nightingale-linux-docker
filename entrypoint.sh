36438314e875f5b9ddf1c071f4fd90b60d57ecd5
Broadband <50155192+Seantourage@users.noreply.github.com>
2025-09-01 19:54:23 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,17 +17,15 @@ WorldSaveName="${WorldSaveName:-Cascade}"
 AdditionalArgs="${AdditionalArgs:-}"
 
 # Check for updates/perform initial installation
-if [ ! -d "/server/AbioticFactor/Binaries/Win64" ] || [[ $AutoUpdate == "true" ]]; then
+if [ ! -d "/server/NWX/Binaries/Win64" ] || [[ $AutoUpdate == "true" ]]; then
     steamcmd \
     +@sSteamCmdForcePlatformType windows \
     +force_install_dir /server \
     +login anonymous \
-    +app_update 2857200 validate \
+    +app_update 3796810 validate \
     +quit
 fi
 
-pushd /server/AbioticFactor/Binaries/Win64 > /dev/null
-wine AbioticFactorServer-Win64-Shipping.exe $SetUsePerfThreads$SetNoAsyncLoadingThread-MaxServerPlayers=$MaxServerPlayers \
-    -PORT=$Port -QueryPort=$QueryPort -ServerPassword=$ServerPassword \
-    -SteamServerName="$SteamServerName" -WorldSaveName="$WorldSaveName" -tcp $AdditionalArgs
+pushd /server/NWX/Binaries/Win64 > /dev/null
+wine NWXServer-Win64-Shipping.exe -PORT=$Port
 popd > /dev/null
